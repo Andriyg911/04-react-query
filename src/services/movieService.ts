3d8cf4b23ee628cc4d821203e23327b1ec093208
Andriy <kykyagoloviy@gmail.com>
2025-10-16 20:16:06 +0300

    Final commit before deploy

---
+++ src/services/movieService.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosResponse } from 'axios';
+import type { Movie } from '../types/movie';
+
+const API_URL = 'https://api.themoviedb.org/3/search/movie';
+const TOKEN = import.meta.env.VITE_TMDB_TOKEN as string;
+
+export interface FetchMoviesParams {
+  query: string;
+  page?: number;
+}
+
+interface MoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export async function fetchMovies(
+  { query, page = 1 }: FetchMoviesParams
+): Promise<Movie[]> {
+  const config = {
+    params: {
+      query,
+      page,
+      include_adult: false,
+      language: 'en-US',
+    },
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  };
+
+  const response: AxiosResponse<MoviesResponse> = await axios.get(API_URL, config);
+  return response.data.results;
+}
